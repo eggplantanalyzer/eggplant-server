6ce448aadf9133eb419cfb9f241a82f6e0e9f38b
eggplantanalyzer <eggplantanalyzer@gmail.com>
2025-02-27 09:37:49 +0200

    port

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,3 +1,9 @@
+# Gunicorn configuration file
+import os
+
+# Port is handled by Render
+port = os.environ.get("PORT", 10000)
+
+bind = f"0.0.0.0:{port}"
 workers = 4
-bind = "0.0.0.0:10000"
 timeout = 120 
