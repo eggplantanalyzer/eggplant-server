8096c3df9c870bad19c8b15b8934e6683cd2692d
eggplantanalyzer <eggplantanalyzer@gmail.com>
2025-02-27 10:22:50 +0200

    .

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,19 +1,21 @@
 # Gunicorn configuration file
 import os
 
-# Get port from environment variable or default to 10000
-port = os.environ.get("PORT", 10000)
+# Get port from environment variable
+port = int(os.environ.get("PORT", 10000))
 
-# Bind to 0.0.0.0 to allow external access
+# Bind configuration
 bind = f"0.0.0.0:{port}"
 
 # Worker configuration
 workers = 4
 worker_class = 'sync'
-worker_connections = 1000
 timeout = 120
 
 # Logging
 accesslog = '-'
 errorlog = '-'
-loglevel = 'info' 
+loglevel = 'debug'  # Changed to debug for more information
+
+# Worker configuration
+worker_connections = 1000 
